2227fc65c21466060a6e391a7daf76612face781
vkavya614 <60350049+vkavya614@users.noreply.github.com>
2020-07-24 11:10:08 +0530

    Create person.java

---
+++ person.java
@@ -0,0 +1,11 @@
+public abstract class Person {
+	String name,roll, collegeName, city;
+	int age;
+	public abstract void setName(String name);
+	public abstract void setAge(int age);
+	public abstract void setCollegeName(String clgName);
+	public abstract void setCity(String city);
+	
+
+
+}
